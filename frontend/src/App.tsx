986536379417dffb3b6f89152d4d04b7fb65a7ba
Samuel Olmos <olmos.samu@gmail.com>
2026-04-14 15:52:50 -0300

    chore: estructura base del frontend (pages)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Home from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
-    <>
+    /*<>
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
@@ -114,7 +115,8 @@ function App() {
 
       <div className="ticks"></div>
       <section id="spacer"></section>
-    </>
+    </>*/
+    <Home />
   )
 }
 
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+    return <h1>Home FACEA</h1>;
+};
+
+export default Home;
