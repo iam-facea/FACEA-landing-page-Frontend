e47c9994b31e1b18f5407d6f6c8c16439eca8ec4
Samuel Olmos <olmos.samu@gmail.com>
2026-05-03 13:35:31 -0300

    chore: eliminar componentes de prueba

--- frontend/src/components/MerchandisingSection.tsx
+++
@@ -1,9 +0,0 @@
-const MerchandisingSection = () => {
-    return (
-        <section>
-            <h2>Merchandising</h2>
-        </section>
-    );
-}
-
-export default MerchandisingSection;
--- frontend/src/components/NewsSection.tsx
+++
@@ -1,9 +0,0 @@
-const NewsSection = () => {
-    return (
-        <section>
-            <h2>News</h2>
-        </section>
-    );
-}
-
-export default NewsSection;
