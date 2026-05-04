fe7f95ac5768d4589b45a923904556075db1dd0c
Samuel Olmos <olmos.samu@gmail.com>
2026-05-04 17:23:33 -0300

    chore: Crear la constante global para las referencias de las secciones para la NavBar

---
+++ frontend/src/constants/navigation.ts
@@ -0,0 +1,7 @@
+// Exportamos la constante en mayúsculas para indicar que es inmutable
+export const NAV_LINKS = [
+  { id: "inicio", label: "Inicio", href: "#inicio" },
+  { id: "nosotros", label: "Nosotros", href: "#nosotros" },
+  { id: "actividades", label: "Novedades", href: "#actividades" },
+  { id: "impacto", label: "Impacto", href: "#impacto" },
+];
